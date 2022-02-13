b43435b09e371e9542c52c0026499a909f4c6f18
TRP <tirth4ur@gmail.com>
2022-02-12 21:18:45 -0500

    Project 1 upload to git

---
+++ Project/PA1/driver.c
@@ -0,0 +1,30 @@
+/*
+Tirth Patel
+CPSC 2310 F21 Section 001
+tirthp@g.clemson.edu
+*/
+#include <stdio.h>
+#include "utils.h"
+#include "EncodeDecode.h"
+#include "ppm.h"
+int main(int argc, char* argv[])
+{
+    checkArgs(argc, 2);
+    FILE* in = fopen(argv[1], "r");
+    checkFile(in);
+    FILE* out = fopen(argv[2], "w");
+    checkFile(out);
+    char* msg = "I hope this works!";
+    encodeMsg(in, out, msg);
+    FILE* dec = fopen(argv[2], "r");
+    checkFile(dec);
+    decodeMsg(dec);
+
+    fclose(in);
+    fclose(out);
+    fclose(dec);
+
+    return 0; //was EXIT_SUCCESS
+}
+
+
